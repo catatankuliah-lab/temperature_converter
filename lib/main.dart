8d58f3159cf696ede07b9292b99a6fccea14759d
Nana <nanasupriatnakursus@gmail.com>
2021-10-17 02:28:53 +0700

    change primarySwatch

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Pineapple's Temprature Converter",
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blue,
       ),
       home: const Home(),
     );
